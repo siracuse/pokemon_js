70d85769db8435b3304392885955d2a7ef6f89f0
SIRACUSE Harichandra <siracuse.harichandra@gmail.com>
2023-01-29 12:06:30 +0100

    système de chat

--- asset.js
+++ asset.js
@@ -5,6 +5,15 @@ function player_choose_pokemon_1() {
     let value = document.getElementById('player1_choose').value;
     pokemon1 = new Pokemon(value);
 
+
+    let contentNode = 'Vous avez choisi '  + pokemon1.name.toUpperCase();
+    document.getElementById('pokemon_fight_chat').appendChild(newRowChat(contentNode));
+    updateScroll();
+
+
+    // document.getElementById('pokemon_fight_chat').appendChild(div);
+    // document.getElementById('pokemon_fight_chat_row').innerHTML = 'Vous avez choisi '  + pokemon1.name.toUpperCase()
+
     document.getElementById('player_choose_pokemon_img_1').src = 'img/pokemon_sprite/' + pokemon1.name + '.gif'
     document.getElementById('player1_battle_pokemon').src = 'img/pokemon_sprite/' + pokemon1.name + '_back.gif'
     document.getElementById('player_info_name_1').innerHTML = pokemon1.name
@@ -19,6 +28,7 @@ function player_choose_pokemon_1() {
     let pokemon_key = Math.floor(Math.random() * 3)
     pokemon2 = new Pokemon(random_pokemon[pokemon_key]);
 
+    // document.getElementById('pokemon_fight_chat_row').innerHTML = 'Votre adversaire a choisi '  + pokemon2.name.toUpperCase()
     document.getElementById('player2_battle_pokemon').src = 'img/pokemon_sprite/' + pokemon2.name + '.gif'
     document.getElementById('player_info_name_2').innerHTML = pokemon2.name
 }
@@ -27,11 +37,19 @@ function player_choose_pokemon_1() {
 function attaquePlayer(pokemon_attaque) {
     let attaque_name = document.getElementById(pokemon_attaque).innerText;
     let attaque1 = new Attaque(attaque_name.toLowerCase());
+    
+    let contentNode = pokemon1.name.toUpperCase() + ' utilise ' + attaque_name;
+    document.getElementById('pokemon_fight_chat').appendChild(newRowChat(contentNode));
+    updateScroll();
+
     let pvLose = calculPvLose(attaque1, 'pokemon1')
     pokemon2.setNewPv(pvLose);
 
     if (pokemon2.pv <= 0) {
         endMatch('pokemon_info_2_pv')
+        let contentNode = 'Vous avez gagné';
+        document.getElementById('pokemon_fight_chat').appendChild(newRowChat(contentNode));
+        updateScroll();
     } else {
         let newWidth = ((pokemon2.pv * 220) / pokemon2.pvMax);
         barrePv(newWidth, 'pokemon_info_2_pv')
@@ -46,18 +64,26 @@ function attaqueBot() {
             let attaque_key = Math.floor(Math.random() * 3)
             let attaque_name = pokemon2[random_attaque[attaque_key]];
             let attaque2 = new Attaque(attaque_name.toLowerCase());
+            
+            let contentNode = pokemon2.name.toUpperCase() + ' utilise ' + attaque_name.toUpperCase();
+            document.getElementById('pokemon_fight_chat').appendChild(newRowChat(contentNode));
+            updateScroll();
+
             let pvLose = calculPvLose(attaque2, 'pokemon2')
             console.log(attaque_name);
             pokemon1.setNewPv(pvLose)
 
             if (pokemon1.pv <= 0) {
                 endMatch('pokemon_info_1_pv')
+                let contentNode = 'Vous avez perdu';
+                document.getElementById('pokemon_fight_chat').appendChild(newRowChat(contentNode));
+                updateScroll();
             } else {
                 let newWidth = ((pokemon1.pv * 220) / pokemon1.pvMax);    
                 barrePv(newWidth, 'pokemon_info_1_pv')
                 document.getElementById('pokemon_info_pv_value_1').innerHTML = pokemon1.pv +'/'+pokemon1.pvMax;
             }
-        }, 400);
+        }, 600);
 }
 
 
@@ -94,4 +120,23 @@ function barrePv(newWidth, pokemon) {
     } else {
         document.getElementById(pokemon).style.backgroundColor = '#70F8A8';
     }
+}
+
+function newRowChat(contentNode) {
+    let div = document.createElement('div');
+    div.id = 'pokemon_fight_chat_row';
+    div.className = 'pokemon_fight_chat_row'
+    let date = new Date();
+
+    let textNode = document.createTextNode(date.getMinutes() + ':' + date.getSeconds() + ' = ' + contentNode);
+    div.appendChild(textNode);
+    return div;
+
+
+
+}
+
+function updateScroll(){
+    let element = document.getElementById("pokemon_fight_chat");
+    element.scrollTop = element.scrollHeight;
 }
